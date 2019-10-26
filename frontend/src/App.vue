7f4fa5519450b6afcd81e5a7e6ee820cdccd6466
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-10-26 16:15:55 +0300

    Fix linting errors

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <Button outline>
-        {{ text }}
-      </Button>
-      <TextField :label="text" v-model="text" />
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+			<Button outline>
+				{{ text }}
+			</Button>
+			<TextField :label="text" v-model="text" />
+		</div>
+		<router-view/>
+	</div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Button from '@/components/Button.vue';
-  import TextField from '@/components/TextField.vue';
+	import Vue from 'vue';
+	import Button from '@/components/Button.vue';
+	import TextField from '@/components/TextField.vue';
 
-  export default Vue.extend({ components: {Button, TextField},data() {
-    return {
-      text: '',
-    };
-  }, });
+	export default Vue.extend({
+		components: {
+			Button,
+			TextField,
+		},
+		data() {
+			return {
+				text: '',
+			};
+		},
+	});
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
--- frontend/src/components/TextField.vue
+++ frontend/src/components/TextField.vue
@@ -1,58 +1,58 @@
 <template>
-  <div class="text-field">
-    <label>{{ label }}</label>
-    <input type="text" :value="value" @input="$emit('input', $event.target.value)" />
-  </div>
+	<div class="text-field">
+		<label>{{ label }}</label>
+		<input type="text" :value="value" @input="$emit('input', $event.target.value)" />
+	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default Vue.extend({
-  props: {
-    label: {
-      type: String,
-    },
-    value: {
-      type: String,
-    }
-  },
+	props: {
+		label: {
+			type: String,
+		},
+		value: {
+			type: String,
+		},
+	},
 });
 </script>
 
 <style lang="scss" scoped>
-  .text-field {
-    border: 1px solid rgba(38, 40, 64, 0.3);
-    box-sizing: border-box;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: .5em 1em;
+	.text-field {
+		border: 1px solid rgba(38, 40, 64, 0.3);
+		box-sizing: border-box;
+		border-radius: 15px;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding: .5em 1em;
 
-    label {
-      font-size: 12px;
-      color: #262840;
-      text-transform: uppercase;
-      font-weight: 600;
-      font-family: 'Open Sans';
-      margin-bottom: .3em;
-    }
+		label {
+			font-size: 12px;
+			color: #262840;
+			text-transform: uppercase;
+			font-weight: 600;
+			font-family: 'Open Sans';
+			margin-bottom: .3em;
+		}
 
-    input {
-      background: none;
-      border: none;
-      outline: none;
-      font-size: 16px;
-      font-family: 'Open Sans';
-      width: 100%;
-    }
+		input {
+			background: none;
+			border: none;
+			outline: none;
+			font-size: 16px;
+			font-family: 'Open Sans';
+			width: 100%;
+		}
 
-    &:focus-within {
-      border-color: #4DB39A;
-      label {
-        color: #4DB39A;
-      }
-    }
-  }
+		&:focus-within {
+			border-color: #4DB39A;
+			label {
+				color: #4DB39A;
+			}
+		}
+	}
 </style>
