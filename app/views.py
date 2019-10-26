30aa510fb88579b482d70e17886336167c5d77dd
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-10-26 11:52:33 +0300

    Return LB variety depending on current user

--- app/views.py
+++ app/views.py
@@ -3,10 +3,12 @@
 from datetime import datetime
 import mimetypes
 import json
+import math
 
 # pylint: disable=import-error
 import requests
 from flask import Blueprint, abort, jsonify, request, current_app
+from flask_login import current_user
 from flask.views import MethodView
 from psycopg2.extras import DateRange
 from sqlalchemy import or_
@@ -14,7 +16,7 @@ import werkzeug
 from werkzeug.exceptions import BadRequestKeyError
 # pylint: enable=import-error
 
-from app.models import (Course, Enrollment, Lesson, LearningBlock, LearningBlockVariety)
+from app.models import (Course, Enrollment, Lesson, LearningBlock, LearningBlockVariety, Account)
 
 api = Blueprint('api', __name__)  # pylint: disable=invalid-name
 
@@ -57,11 +59,36 @@ def get_lessons(course_id):
 	lessons = Lesson.query.filter_by(course_id=course_id).order_by(Lesson.order.asc()).all()
 	return jsonify(lessons)
 
+def student_quiz_average(student, course):
+	enrollment = Enrollment.query.filter_by(enrollee_id=student.id, course_id=course.id).one()
+	lessons = Lesson.query.filter_by(enrollment_id=enrollment.id).all()
+	total_grade = sum(lesson.quiz_grade for lesson in lessons) / len(lessons)
+	return total_grade
+
 @api.route('/course/<int:course_id>/lesson/<int:lesson_id>')
 def get_lesson(course_id, lesson_id):
+	user = None
+	if current_user.is_authenticated():
+		user = Account.query.get_or_404(current_user.get_id())
+	course = Course.query.get_or_404(course_id)
 	lesson = Lesson.query.get_or_404(lesson_id)
 	blocks = LearningBlock.query.filter_by(lesson_id=lesson_id).all()
-	varieties = [LearningBlockVariety.query.filter_by(block_id=block.id).all() for block in blocks]
+	varieties = []
+	if user is None:
+		for block in blocks:
+			block_vars = LearningBlockVariety.query.filter_by(block_id=block.id).all()
+			varieties.push(block_vars[len(block_vars)//2])
+	elif user.is_teacher:
+		for block in blocks:
+			block_vars = LearningBlockVariety.query.filter_by(block_id=block.id).all()
+			varieties.append(block_vars)
+	else:
+		for block in blocks:
+			block_vars = LearningBlockVariety.query.filter_by(block_id=block.id)
+			avg_grade = student_quiz_average(user, course)
+			difficulty = math.ceil(avg_grade * len(block_vars))
+			block_var = block_vars.filter_by(difficulty=difficulty).one()
+			varieties.push(block_var)
 
 	obj = {
 		'name': lesson.name,
