54f26b70a49d407aeb190cd5df5baea2a2ebdbfc
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-10-27 08:51:29 +0300

    Group statistics by range

--- frontend/src/components/BarChart.vue
+++ frontend/src/components/BarChart.vue
@@ -1,22 +1,20 @@
 <script lang="ts">
 	import Vue from 'vue';
-	import { Bar } from 'vue-chartjs';
+	import { Bar, mixins } from 'vue-chartjs';
 
 	export default Vue.extend({
 		extends: Bar,
-		props: ['labels', 'students'],
+		mixins: [mixins.reactiveData],
+		props: ['label', 'students'],
 		data() {
 			return {
-				chartData: {
-					labels: this.labels,
-					datasets: this.students.map((student: string) => ({
-						backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16),
-						label: student,
-						data: [...Array(6)].map(() => this.getRandomInt(0, 10)),
-					})),
-				},
 				options: {
-
+					scales: {
+						yAxes: [{
+							display: true,
+							ticks: {beginAtZero: true},
+						}],
+					},
 				},
 			};
 		},
@@ -25,8 +23,39 @@
 				return Math.floor(Math.random() * (high - low)) + low;
 			},
 		},
-		mounted() {
-			this.renderChart(this.chartData, this.options);
+		watch: {
+			label: {
+				handler(newLabel) {
+					if(this.chartData == null) {
+						Vue.set(this, 'chartData', {});
+					}
+
+					const nums: number[] = this.students.map((_: string) => this.getRandomInt(0,10));
+					this.chartData.labels = ['0-2', '3-4', '5-6', '7-8', '9-10'];
+					const data = [
+						nums.filter(num => num >= 0 && num <= 2).length,
+						nums.filter(num => num >= 3 && num <= 4).length,
+						nums.filter(num => num >= 5 && num <= 6).length,
+						nums.filter(num => num >= 7 && num <= 8).length,
+						nums.filter(num => num >= 9 && num <= 10).length,
+					];
+
+					this.chartData.datasets = [{
+						backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16),
+						label: 'student count',
+						data,
+					}];
+
+					/* this.chartData.labels = [newLabel];
+					this.chartData.datasets = this.students.map((student: string) => ({
+						backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16),
+						label: student,
+						data: [this.getRandomInt(1, 10)],
+					})); */
+					this.renderChart(this.chartData, this.options);
+				},
+				immediate: true,
+			},
 		},
 	});
 </script>
